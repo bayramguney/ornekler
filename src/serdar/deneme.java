465edfc87a8c197591b4a7450696bc14e481edcd
bayramguney <bayramguney@yahoo.com>
2020-08-23 21:45:14 -0400

    master removedup ekledi

--- src/serdar/deneme.java
+++ src/serdar/deneme.java
@@ -7,30 +7,13 @@ import java.util.List;
 
 public class deneme {
     public static void main(String[] args) {
-        String password = "123abc#$%A";
-        System.out.println(isValid(password));
-    }
-
-    public static boolean isValid(String password) {
-
-        String uppercase=".*[A-Z].*";
-        String lowercase=".*[a-z].*";
-        String number=".*[0-9].*";
-        String special=".*[ -/, :-@].*";
-
-        boolean hasUpper=password.matches(uppercase),
-                hasLower=password.matches(lowercase),
-                hasNumber=password.matches(number),
-                hasSpecial=password.matches(special),
-        isValid=false;
-        if(password.length()>=6 && !password.contains(" ")){
-            if(hasLower && hasNumber && hasUpper && hasSpecial)
-            isValid=true;
+        String str = "AAABBBCCC";
+        String nonDub="";
+        for (int i = 0; i <str.length() ; i++) {
+            if(!nonDub.contains(str.substring(i,i+1))){
+                nonDub+=str.substring(i,i+1);
+            }
         }
-
-
-
-
-    return isValid;
+        System.out.println(nonDub);
     }
 }
